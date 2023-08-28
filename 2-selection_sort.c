171ac026da9e1b6c226c9cc4717db44f2b63bbb8
caramelinvestor <caramelinvestor@gmail.com>
2023-08-28 08:29:42 -0700

    2. Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,19 +1,29 @@
 #include "sort.h"
 
 /**
- * selection_sort - Function that sorts an array of integers in
- * ascending order using Selection Sort.
+ * selection_sort - Function that sorts an array of integers
+ * in ascending order using Selection Sort.
  *
  * @array: The array to be sorted.
  * @size: Number of elements in the array.
  */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
+	int *temp_array = malloc(sizeof(int) * size);
 
 	if (array == NULL || size < 2)
 		return;
 
+	if (temp_array == NULL)
+		return;
+
+		
+	for (i = 0; i < size; i++)
+	{
+		temp_array[i] = array[i];
+	}
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -21,22 +31,23 @@ void selection_sort(int *array, size_t size)
 
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_idx])
+			if (temp_array[j] < temp_array[min_idx])
 				min_idx = j;
 		}
+
 		if (min_idx != i)
 		{
-			int temp = array[i];
-
-			array[i] = array[min_idx];
-			array[min_idx] = temp;
-
-			_printf("%d", array[0]);
-			for (j = 1; j < size; j++)
-			{
-				_printf(", %d", array[j]);
-			}
-			_printf("\n");
+			int temp = temp_array[i];
+			temp_array[i] = temp_array[min_idx];
+			temp_array[min_idx] = temp;
 		}
 	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = temp_array[i];
+	}
+
+	free(temp_array);
 }
+
